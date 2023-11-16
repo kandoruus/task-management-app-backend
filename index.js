e5e71db138110b06afb15ce8d4f39b4f27ab012a
caleb <1d0nt3v3nl1ftbr0@gmail.com>
2023-11-16 13:20:39 -0500

    bug fix: crash when creating new task

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get("/", (req, res) => {
 });
 
 const getDataFromInputs = (inputs) => {
-  let { name, description, status, priority } = inputs.data;
+  let { name, description, status, priority } = JSON.parse(inputs.data);
   status = status === "" ? "Not Started" : status;
   priority = priority === "" ? "Low" : priority;
   return {
